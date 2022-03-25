2d31305a91496fc04cc609c4a192c9b0c181f6c7
Gökhan FIRATLI <gokhanfiratli@hotmail.com>
2022-03-25 17:48:15 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int time = DateTime.Now.Hour;
+
+if (time >=6 && time <11)
+    Console.WriteLine("Günadın!");
+else if (time <= 18)
+    Console.WriteLine("İyi Günler!");
+else
+    Console.WriteLine("İyi Geceler!");
+
+string sonuc = time >= 6 ? "Günaydın!" : time <=18 ? "İyi Günler!" : "İyi Geceler!";
+    Console.WriteLine(sonuc);
